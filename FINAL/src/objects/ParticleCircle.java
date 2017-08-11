f8ef4b268c1b129fdbdd83bd2fed8803f6b4ced3
tk46 <torsten.krix@googlemail.com>
2017-08-11 22:11:18 +0200

    Update ParticleCircle.java

--- FINAL/src/objects/ParticleCircle.java
+++ FINAL/src/objects/ParticleCircle.java
@@ -3,9 +3,12 @@ package objects;
 import java.util.ArrayList;
 import controller.CustomAnimation;
 import controller.CustomAnimationCircle;
+import controller.CustomAnimationColor;
+import controller.CustomAnimationVelocity;
 import de.uulm.mi.gdg.Equalizer;
 import processing.core.PApplet;
 import processing.core.PVector;
+import processing.core.PShape;
 
 public class ParticleCircle extends Equalizer implements Entity {
 	private static PApplet canvas;
@@ -16,36 +19,67 @@ public class ParticleCircle extends Equalizer implements Entity {
 	private PVector position;
 
 	public ParticleCircle(float r, float t, PApplet papa, ArrayList<CustomAnimation> anis,
-			ArrayList<CustomAnimationCircle> anis2, int i) {
+			ArrayList<CustomAnimationCircle> anis2,ArrayList<CustomAnimationVelocity> anis3, ArrayList<CustomAnimationColor> anis4, int i) {
 		radius = 0;
-		c = color(random(25, 200), random(50, 200), random(120, 255), random(5, 255));
-		transparency = 200;
+		self = canvas.createShape();
+                self.beginShape();
+		self.stroke(anis4.get(i).getCR(),anis4.get(i).getCG(),anis4.get(i).getCB(),transparency);
+                self.strokeWeight(anis2.get(i).getWeight());
+		self.vertex(0, 0);
+                self.vertex(1, 0);
+
+                self.endShape();
+        
+
+		transparency = 200f;
 
 		canvas = papa;
 
-		position = new PVector(canvas.second()%2==0?1000:canvas.random(canvas.width/1.2f,1100), anis2.get(i).getYPos());
-		velocity = new PVector(random(-3,-1), random(-1, 1));
+		position = new PVector(anis2.get(i).getXPos(), anis2.get(i).getYPos());
+		velocity = new PVector(anis3.get(i).getX(),anis3.get(i).getY());
 
 	}
 
 	public void update() {
 		position.add(velocity);
-		if ((position.x > canvas.width) || (position.x < 0)) {
-			velocity.x = velocity.x * -1 ;
-		}
-		if ((position.y > canvas.height) || (position.y < 0)) {
-			velocity.y = velocity.y * -1 ;
-		}
-		radius++;
-		if (transparency > 0) {
-			transparency = (float) (transparency - 0.9);
-		}
+    	        transparency--;
 	}
 
 	public void display() {
-		canvas.fill(c, transparency - 29);
-		canvas.stroke(c, transparency );
-		canvas.strokeWeight(1.5f);
-		canvas.ellipse(position.x, position.y, radius, radius);
+		canvas.pushMatrix();
+                canvas.translate(position.x, position.y);
+                 canvas.shape(self);
+                canvas.popMatrix();
 	}
+	public boolean checkEdges(){
+    	if (position.y >= canvas.height+200 && velocity.y > 0) {
+            return true;
+        } else if (position.y <= -200 && velocity.y < 0) {
+            return true;
+        } else if (position.x >= canvas.width+200 && velocity.x > 0) {
+            return true;
+        } else if (position.x <= -200 && velocity.x < 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    public void Bouncing(){
+    	if (position.y >= canvas.height-12.5 && velocity.y > 0) {
+            velocity.y = velocity.y*(-1);
+        } else if (position.y <= 12.5 && velocity.y < 0) {
+            velocity.y = velocity.y*(-1);
+        } else if (position.x >= canvas.width-12.5 && velocity.x > 0) {
+        	velocity.x = velocity.x*(-1);
+        } else if (position.x <= 12.5 && velocity.x < 0) {
+        	velocity.x = velocity.x*(-1);
+        }
+        
+    }
+	
+	
+	
+
+	
+	
 }

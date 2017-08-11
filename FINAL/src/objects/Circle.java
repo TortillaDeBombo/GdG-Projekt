60e460d15370b5a0eefdfb221c60cb69bdf1fbb3
tk46 <torsten.krix@googlemail.com>
2017-08-11 22:05:01 +0200

    Update Circle.java

--- FINAL/src/objects/Circle.java
+++ FINAL/src/objects/Circle.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import controller.AniImporter;
 import controller.CustomAnimation;
 import controller.CustomAnimationCircle;
+import controller.CustomAnimationColor;
+import controller.CustomAnimationVelocity;
 import de.looksgood.ani.Ani;
 import processing.core.PApplet;
 import processing.core.PConstants;
@@ -20,6 +22,8 @@ public class Circle implements Entity {
 	private static ArrayList<ParticleCircle> deadCircles;
 	private ArrayList<CustomAnimation> anis;
 	private ArrayList<CustomAnimationCircle> anis2;
+	private ArrayList<CustomAnimationVelocity> anis3;
+	private ArrayList<CustomAnimationColor> anis4;
 	private CustomAnimation ani;
 	private int Circles = 0;
 	int i = 0;
@@ -30,18 +34,20 @@ public class Circle implements Entity {
 		deadCircles = new ArrayList<ParticleCircle>();
 		canvas = _parent;
 
-		startAnimation();
+		startAnimation(Farbe);
 
 	}
 
 	public void startAnimation() {
 		anis = AniImporter.importAnimation(canvas, "./data/timing/timing.json", "Circles");
 		anis2 = AniImporter.importAnimationData(canvas, "./data/timing/timing.json", "Data");
+		anis3 = AniImporter.importAnimationVelocity(canvas, "./data/timing/timing.json", "Velocity");
+		anis4 = AniImporter.importAnimationColor(canvas, "./data/timing/timing.json", Farbe);
 	}
 
 	public void spawn() {
 		for (; Circles > 0; Circles--) {
-			circles.add(new ParticleCircle(0,200,canvas, anis, anis2, i));
+			circles.add(new ParticleCircle(canvas, anis, anis2, anis3, anis4, i));
 			if (i < anis2.size()) {
 				i++;
 			}
@@ -58,6 +64,7 @@ public class Circle implements Entity {
 	public void update() {
 		for (ParticleCircle p : circles) {
 			p.update();
+			p.Bouncing;
 		}
 		circles.removeAll(deadCircles);
 		deadCircles.clear();
